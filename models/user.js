574d0b9a9f208cd1ec30590e59100fad79dfafa9
Christopher Arguello <kiko_arguello@hotmail.com>
2018-10-24 21:34:31 -0400

    Express generated app

---
+++ models/user.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+const Schema   = mongoose.Schema;
+
+const userSchema = new Schema({
+  email:  { type: String, required: true, unique: true },
+  password: String,
+  role: { type: String, default: "user", enum: ["admin", "user"] },
+  resetPasswordToken: String,
+  resetPasswordExpires: Date,
+  isAccountActivated: { type: Boolean, default: false },
+  accountActivationToken: String,
+}, {
+  timestamps: {
+    createdAt: "created_at",
+    updatedAt: "updated_at"
+  }
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
---
+++ public/javascripts/main.js
@@ -0,0 +1,7 @@
+// Examine the Document Object //
+
+// console.dir(document);
+console.log(document.domain);
+console.log(document.URL);
+console.log(document.title);
+document.title = "FitApp"
